81f71959c013f5e5b9d67a2ebae46302c9931458
Khang Zheng Ng <khang.ng@weareadaptive.com>
2022-03-02 13:10:28 -0500

    initial approach to finish user controller & service

--- auctionhouse/src/main/java/com/weareadaptive/auction/controller/UserController.java
+++ auctionhouse/src/main/java/com/weareadaptive/auction/controller/UserController.java
@@ -1,9 +1,19 @@
 package com.weareadaptive.auction.controller;
 
+import com.weareadaptive.auction.controller.dto.CreateUserRequest;
+import com.weareadaptive.auction.controller.dto.UpdateUserRequest;
+import com.weareadaptive.auction.controller.dto.UserResponse;
+import com.weareadaptive.auction.exception.ExceptionHandlerControllerAdvice;
+import com.weareadaptive.auction.model.BusinessException;
+import com.weareadaptive.auction.model.User;
 import com.weareadaptive.auction.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -13,5 +23,55 @@ public class UserController {
 
   public UserController(UserService userService) {
     this.userService = userService;
+  }
+
+  @PostMapping()
+  @ResponseStatus(HttpStatus.CREATED)
+  UserResponse createUser(@RequestBody @Valid CreateUserRequest createUserRequest) {
+    User user = userService.create(
+            createUserRequest.username(),
+            createUserRequest.password(),
+            createUserRequest.firstName(),
+            createUserRequest.lastName(),
+            createUserRequest.organisation());
+
+    return new UserResponse(user.getId(), user.getUsername(), user.getFirstName(), user.getLastName(), user.getOrganisation());
+  }
+
+  @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  Optional<User> getUserById(@PathVariable("id") int id) {
+    Optional<User> user = userService.getById(id);
+
+    if (user.isPresent()) {
+
+    }
+
+    return userService.getById(id);
+  }
+
+  @PutMapping("/{id}")
+  Optional<User> updateUserById(@PathVariable("id") int id, @RequestBody @Valid UpdateUserRequest updateUserRequest, HttpServletResponse servletResponse) {
+    Optional<User> user = userService.updateById(id, updateUserRequest.firstName(), updateUserRequest.lastName(), updateUserRequest.organisation());
+
+    if (user.isPresent()) {
+      servletResponse.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      servletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    return user;
+  }
+
+  @PutMapping("/{id}/block")
+  void blockUserById(@PathVariable("id") int id, HttpServletResponse servletResponse) {
+    Optional<User> user = userService.getById(id);
+
+    if (user.isEmpty()) {
+      servletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } else if (!user.get().isBlocked()) {
+      user.get().block();
+      servletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    }
   }
 }
--- auctionhouse/src/main/java/com/weareadaptive/auction/service/UserService.java
+++ auctionhouse/src/main/java/com/weareadaptive/auction/service/UserService.java
@@ -4,16 +4,37 @@ import com.weareadaptive.auction.model.User;
 import com.weareadaptive.auction.model.UserState;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
-  private final UserState userState;
+    private final UserState userState;
 
-  public UserService(UserState userState) {
-    this.userState = userState;
-  }
+    public UserService(UserState userState) {
+        this.userState = userState;
+    }
 
-  public User create(String username, String password, String firstName, String lastName,
-                     String organisation) {
-    throw new UnsupportedOperationException();
-  }
+    public User create(String username, String password, String firstName, String lastName,
+                       String organisation) {
+        var user = new User(userState.nextId(), username, password, firstName, lastName, organisation);
+        userState.add(user);
+
+        return user;
+    }
+
+    public Optional<User> getById(int id) {
+        return userState.stream().filter(u -> u.getId() == id).findFirst();
+    }
+
+    public Optional<User> updateById(int id, String firstName, String lastName, String organisation) {
+        Optional<User> user = getById(id);
+
+        if (user.isPresent()) {
+            user.get().setFirstName(firstName);
+            user.get().setLastName(lastName);
+            user.get().setOrganisation(organisation);
+        }
+
+        return user;
+    }
 }
