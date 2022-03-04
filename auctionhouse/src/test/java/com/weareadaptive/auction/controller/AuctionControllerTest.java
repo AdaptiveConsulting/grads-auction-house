9a67cb71224d632737cc5a1fd9884dbbc0161053
Khang Zheng Ng <khang.ng@weareadaptive.com>
2022-03-04 17:51:39 -0500

    improve auction controller test

--- auctionhouse/src/test/java/com/weareadaptive/auction/controller/AuctionControllerTest.java
+++ auctionhouse/src/test/java/com/weareadaptive/auction/controller/AuctionControllerTest.java
@@ -1,11 +1,27 @@
 package com.weareadaptive.auction.controller;
 
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.comparesEqualTo;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
 import com.github.javafaker.Faker;
 import com.weareadaptive.auction.TestData;
+import com.weareadaptive.auction.controller.dto.BidAuctionRequest;
 import com.weareadaptive.auction.controller.dto.CreateAuctionRequest;
+import com.weareadaptive.auction.model.AuctionLot;
 import com.weareadaptive.auction.service.AuctionLotService;
 import com.weareadaptive.auction.service.UserService;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,18 +29,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-
-import static com.weareadaptive.auction.TestData.ADMIN_AUTH_TOKEN;
-import static io.restassured.RestAssured.given;
-import static java.lang.String.format;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuctionControllerTest {
@@ -86,10 +90,9 @@ public class AuctionControllerTest {
         .statusCode(BAD_REQUEST.value());
   }
 
-  @DisplayName("Get should return an open auction by id")
+  @DisplayName("Get should return a full OPENED auction for owner")
   @Test
   public void get_returnAnAuctionById() {
-    var user = testData.user1();
     var auctionLot = testData.getAuction_user1();
 
     given()
@@ -99,27 +102,33 @@ public class AuctionControllerTest {
         .when()
         .get("/auctions/{id}")
         .then()
+        .log().all()
         .statusCode(HttpStatus.OK.value())
         .body("id", greaterThan(0))
-        .body("owner", equalTo(user.getUsername()))
         .body("symbol", equalTo(auctionLot.getSymbol()))
         .body("minPrice", equalTo((float) auctionLot.getMinPrice()))
-        .body("quantity", equalTo(auctionLot.getQuantity()));
+        .body("quantity", equalTo(auctionLot.getQuantity()))
+        .body("bids", equalTo(auctionLot.getBids()));
   }
 
-  @DisplayName("get return 403 when server refuse a user to access others' auctions")
+  @DisplayName("Get should return basic info of auction if it's regular user")
   @Test
   public void get_return403WhenGetOthersAuctionLots() {
+    var auctionLot = testData.getAuction_user2();
+
     given()
         .baseUri(uri)
-        .header(AUTHORIZATION, testData.user2Token())
-        .contentType(ContentType.JSON)
-        .pathParam("id", testData.getAuction_user1().getId())
+        .header(AUTHORIZATION, testData.user1Token())
+        .pathParam("id", auctionLot.getId())
         .when()
         .get("/auctions/{id}")
         .then()
-        .statusCode(UNAUTHORIZED.value())
-        .body("message", containsString("user is not authorized"));
+        .log().all()
+        .statusCode(HttpStatus.OK.value())
+        .body("id", greaterThan(0))
+        .body("symbol", equalTo(auctionLot.getSymbol()))
+        .body("minPrice", equalTo((float) auctionLot.getMinPrice()))
+        .body("quantity", equalTo(auctionLot.getQuantity()));
   }
 
   @DisplayName("get return 404 when a user get a nonexistent auction")
@@ -129,7 +138,7 @@ public class AuctionControllerTest {
     given()
         .baseUri(uri)
         .header(AUTHORIZATION, testData.user1Token())
-        .pathParam("id", auctionLotService.getAuctionState().getOwnerIndex().size())
+        .pathParam("id", auctionLotService.getAuctionState().getAuctionIndex().size() + 1)
         .when()
         .get("/auctions/{id}")
         .then()
@@ -152,16 +161,34 @@ public class AuctionControllerTest {
         .when()
         .get("/auctions")
         .then()
+        .log().all()
         .statusCode(HttpStatus.OK.value())
         .body(find1 + "id", equalTo(auctionLot1.getId()))
-        .body(find1 + "owner", equalTo(user1.getUsername()))
         .body(find1 + "symbol", equalTo(auctionLot1.getSymbol()))
         .body(find1 + "minPrice", equalTo((float) auctionLot1.getMinPrice()))
         .body(find1 + "quantity", equalTo(auctionLot1.getQuantity()))
         .body(find2 + "id", equalTo(auctionLot2.getId()))
-        .body(find2 + "owner", equalTo(user2.getUsername()))
         .body(find2 + "symbol", equalTo(auctionLot2.getSymbol()))
         .body(find2 + "minPrice", equalTo((float) auctionLot2.getMinPrice()))
         .body(find2 + "quantity", equalTo(auctionLot2.getQuantity()));
+  }
+
+  @DisplayName("Bid by auction id should return auction and created bid")
+  @Test
+  public void bid_shouldReturnAuctionBid() {
+    var bidRequest = new BidAuctionRequest(
+        faker.random().nextInt(100) + 1,
+        faker.random().nextDouble());
+
+    given()
+        .baseUri(uri)
+        .header(AUTHORIZATION, testData.user1Token())
+        .contentType(ContentType.JSON)
+        .pathParam("id", testData.getAuction_user2().getId())
+        .body(bidRequest)
+        .when()
+        .post("/auctions/{id}/bid")
+        .then()
+        .log().all();
   }
 }
